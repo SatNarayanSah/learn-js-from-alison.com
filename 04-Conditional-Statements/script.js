4e0744225b36cf30a4646c7ced262cf1635cfac9
Satyam Sah <satnarayan5166@gmail.com>
2025-03-02 09:55:33 +0545

    conditional statements

---
+++ 04-Conditional-Statements/script.js
@@ -0,0 +1,9 @@
+let direction = "left";
+
+if (direction == "right") {
+  console.log("You are goint to red planet");
+} else if (direction == "left") {
+  console.log("Your are going to blue planet");
+} else {
+  console.log("The Space ship is crashing");
+}
