370ddca5a89d853b4c5c7852251a3a31c2b6806d
Satyam Sah <satnarayan5166@gmail.com>
2025-03-05 16:51:19 +0545

    dom api

--- 11-Array/scripts.js
+++ 11-Array/scripts.js
@@ -4,14 +4,23 @@ while (i < scores.length) {
   scores[i]++;
   i++;
 }
-console.log(scores);
+// console.log(scores);
 for (let i = 0; i < scores.length; i++) {
   scores[i]++;
 }
-console.log(scores);
+// console.log(scores);
 
 scores.forEach(function (entry, index) {
-  //note: in foreach method the fiest parameter is alwyas entry copy data of an array and second parameter is index number of an array
+  //note: in foreach method have 3 paramets the first parameter is alwyas entry copy data of an array and second parameter is index number of an array and the last one is the array
   scores[index]++;
 });
-console.log(scores);
+// console.log(scores);
+
+
+
+
+// multidimension array
+
+let a = [[1,2,3], [4,5,6,7]]
+a[1][2]=100
+console.log(a[1][2])
---
+++ 12-DOM-API/script.js
@@ -0,0 +1,6 @@
+let sign = document.getElementById('sign');
+console.log(sign)
+console.log(sign.textContent);
+sign.textContent = "You have to become Demon king"
+sign.innerHTML = "The Monster"
+sign.style.fontSize= '10rem'
