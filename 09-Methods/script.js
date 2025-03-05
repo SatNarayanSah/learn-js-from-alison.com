9f6e7993a005218cd71b705b1f6fb9cd8f931fa9
Satyam Sah <satnarayan5166@gmail.com>
2025-03-05 15:04:55 +0545

    Methods

---
+++ 09-Methods/script.js
@@ -0,0 +1,28 @@
+const Player = {
+  Health: 45,
+  Fun: 7,
+  play: function () {
+    console.log("Playing game");
+    this.Fun += 10;
+    console.log("Fun:", this.Fun);
+  },
+  eat: function (food) {
+    if (food == "Apple") {
+      console.log("Eat Apple");
+      this.Health += 10;
+      console.log("The pets Healths:", this.Health);
+    } else if (food == "Canday") {
+      console.log("eat Canday");
+      this.Health -= 5;
+      this.Fun += 10;
+      console.log("Health:", this.Health, "Fun:", this.Fun);
+    }
+  },
+};
+console.log(Player);
+Player.eat("Canday");
+Player.play();
+console.log(Player);
+
+
+// NOTE: Methods declaration:- Function declare as an object property use this Keyword to acces "the current object" and its Property
