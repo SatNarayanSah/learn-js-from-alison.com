35256c17a4a998846483b56a88af2c432c758321
Satyam Sah <satnarayan5166@gmail.com>
2025-03-05 14:29:39 +0545

    objects

---
+++ 08-Objects/sripts.js
@@ -0,0 +1,15 @@
+let player = { 
+    name: "player1",
+    Score: 565,
+    Health: 54,
+    isActive: true,
+    OutFit:{
+        Color:"Green",
+        Size:"xxl",
+        Cost:"450",
+        isNew:true
+    }
+ };
+// console.log(player);
+ player.OutFit.Size = 'M'
+ console.log(player)
